f0a8b9873462e95a209d5c48886e413f6bdcdab3
priyaanshayy <priyanshayyy@gmail.com>
2024-03-12 00:22:26 +0530

    commit

--- client/src/pages/SubscriptionPage.jsx
+++ client/src/pages/SubscriptionPage.jsx
@@ -96,7 +96,7 @@ const PricingCards = () => {
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
               </svg>
             </div>
-            <h1 class="text-4xl text-center font-bold">$45.00</h1>
+            <h1 class="text-4xl text-center font-bold">$30.00</h1>
             <p class="px-4 text-center text-sm ">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem</p>
             <ul class="text-center">
               <li><a href="#" class="font-semibold">It is a long established</a></li>
